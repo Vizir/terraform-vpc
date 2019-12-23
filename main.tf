362f732eebdc0237306885bad53f192cc8279de8
Fabricio Campos <fabricio@vizir.com.br>
2019-12-23 18:54:15 -0300

    fix syntax errors for terraform 0.12

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}"
   }
 }
@@ -15,7 +15,7 @@ resource "aws_subnet" "external" {
   map_public_ip_on_launch = true
   vpc_id                  = "${aws_vpc.main.id}"
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}-${count.index+1}"
   }
 }
@@ -26,7 +26,7 @@ resource "aws_subnet" "private" {
   count                   = "${length(var.private_subnets)}"
   vpc_id                  = "${aws_vpc.main.id}"
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}-private-${count.index+1}"
   }
 }
@@ -34,51 +34,51 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "main" {
   vpc_id = "${aws_vpc.main.id}"
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}"
   }
-}
-
-resource "aws_route" "external" {
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.main.id}"
-  route_table_id         = "${aws_vpc.main.main_route_table_id}"
 }
 
 resource "aws_route_table" "private" {
   count  = "${length(var.private_subnets) > 0 ? 1 : 0}"
   vpc_id = "${aws_vpc.main.id}"
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}-private"
   }
 }
 
+resource "aws_route" "external" {
+  count                  = "${length(aws_route_table.private)}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.main.id}"
+  route_table_id         = "${aws_route_table.private[count.index].id}"
+}
+
 resource "aws_route_table_association" "private" {
-  count          = "${length(var.private_subnets)}"
-  route_table_id = "${aws_route_table.private.id}"
+  count          = "${length(aws_route_table.private)}"
+  route_table_id = "${aws_route_table.private[count.index].id}"
   subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
 }
 
 resource "aws_eip" "nat" {
-  count = "${var.create_nat_gateway ? 1 : 0}"
   vpc   = true
 
-  tags {
+  tags = {
     Name = "${var.vpc_name}-nat"
   }
 }
 
 resource "aws_nat_gateway" "private" {
-  allocation_id = "${aws_eip.nat.0.id}"
-  count         = "${var.create_nat_gateway ? 1 : 0}"
+  allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.external.0.id}"
 
   depends_on = ["aws_internet_gateway.main"]
 }
 
 resource "aws_route" "nat_gateway" {
+  count                  = "${length(aws_route_table.private)}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.private.id}"
-  route_table_id         = "${aws_route_table.private.id}"
+  nat_gateway_id         = "${aws_nat_gateway.private[count.index].id}"
+  route_table_id         = "${aws_route_table.private[count.index].id}"
 }
